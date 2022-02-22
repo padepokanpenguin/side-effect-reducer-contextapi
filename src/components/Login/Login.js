edf5713e4e283ddd3022fa3a46469544683dec59
Aria Nur Jamal <jamal.psw@gmail.com>
2022-02-22 14:33:15 +0700

    changing props with ctx

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -41,6 +42,8 @@ const Login = (props) => {
     value: '',
     isValid: null,
   })
+  
+  const ctx = useContext(AuthContext)
 
   const {isValid: emailIsValid} = emailState;
   const {isValid: passwordIsValid}= passwordState;
@@ -87,7 +90,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
